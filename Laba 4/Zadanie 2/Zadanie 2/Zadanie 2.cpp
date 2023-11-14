e7253887f3e9515e36653844475df12203f5084c
Андрей Плетнев <pletnev2134@gmail.com>
2023-11-14 17:15:00 +0300

    Add project files.

---
+++ Laba 4/Zadanie 2/Zadanie 2/Zadanie 2.cpp
@@ -0,0 +1,22 @@
+﻿//Дано семизначное число. Определить на сколько сумма цифр, стоящих на нечетных позициях, меньше самого числа.
+#include<locale.h>
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int    chislo, chislo1, chislo2, chislo3, chislo4, chislo5, chislo6, chislo7, summa, otvet;
+	printf("Введите 7-значное число:"); scanf_s("%d", &chislo);//1234567
+	chislo1 = chislo % 10;//1 цифра
+	chislo2 = (chislo % 100) / 10;//2 цифра
+	chislo3 = (chislo % 1000) / 100;//3 цифра
+	chislo4= (chislo % 10000) / 1000;//4 цифра
+	chislo5= (chislo % 100000) / 10000;//5 цифра
+	chislo6= (chislo % 1000000) / 100000;//6 цифра
+	chislo7 = chislo / 1000000;//7 цифра
+	summa = chislo1 + chislo3 + chislo5 + chislo7;
+	otvet = chislo - summa;
+	printf("Ответ:%d\n", otvet);
+	system("pause");
+	return 0;
+}
---
+++ Laba 5/Zadanie 2.1/Zadanie 2.1/Zadanie 2.1.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<locale.h>
+#include<math.h>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	//Ввод переменных
+	double x, m;
+	printf("Введите x:"); scanf_s("%lf", &x);
+	//Проверка условий
+	if (x < -1) (m = 4);
+	else if ((x < 10) && (x >= -1)) m = 6.0 * tan(x);
+	else if (x > 10) m = 3 / (2 * x);
+	system("Pause");
+	return 0;
+}
